1e3e321ecc7b043e91999967363a01040f8e1514
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2026-07-28 20:55:21 +0300

    chore: initialize spring boot backend

---
+++ backend/terzi-murat-backend/src/main/java/com/terzimurat/terzimuratbackend/TerziMuratBackendApplication.java
@@ -0,0 +1,13 @@
+package com.terzimurat.terzimuratbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerziMuratBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerziMuratBackendApplication.class, args);
+    }
+
+}
---
+++ backend/terzi-murat-backend/src/test/java/com/terzimurat/terzimuratbackend/TerziMuratBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terzimurat.terzimuratbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerziMuratBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
